a23936401459fc6432c28907dcb2df454c24daed
Riya <hiyoriorria@gmail.com>
2022-12-15 03:48:03 +0900

    add logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,31 @@
 import type { AppProps } from 'next/app';
 
+import Image from 'next/image';
 import { usePageView } from '../hooks/usePageView';
-
 function MyApp({ Component, pageProps }: AppProps) {
     // Google Analytics の PV をカウントするイベント
     usePageView();
 
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />;
+            <div
+                style={{
+                    width: '100%',
+                }}>
+
+                    <Image
+                    style={{
+                        width: '25%',
+                        margin: '0 auto',
+                    }}
+                        src="/img/logo_wide.png"
+                        alt="logo"
+                        width={ 500 }
+                        height={ 500 }
+                    />
+            </div>
+        </>
+    );
 }
 export default MyApp;
